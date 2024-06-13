d64161c062893524b7ec7242508b8522590b89de
Sahul54 <ksahul9576@gmail.com>
2024-06-13 20:59:50 +0530

    update  course, rating and review

--- StudyNotion/controllers/Course.js
+++ StudyNotion/controllers/Course.js
@@ -2,6 +2,7 @@ const Course = require("../models/Course");
 const Tag = require("../models/Category");
 const User = require("../models/User");
 const {uploadImageToCloudinary} = require("../utils/imageUploader");
+const { validatePaymentVerification } = require("razorpay/dist/utils/razorpay-utils");
 
 // createCourse handler function
 exports.createCourse = async (req, res) =>{
@@ -115,5 +116,63 @@ exports.showAllCourses = async (req, res) =>{
             message: 'Cannot fetch Course data',
             error: error.message,
         })
+    }
+}
+
+
+// getCourseDetails
+
+exports.getCourseDetails = async (req, res) => {
+    try{
+        // get id
+        const {courseId} = req.body;
+
+        // find course details
+        const courseDetails = await Course.find(
+            {_id: courseId}
+            .populate(
+                {
+                    path: "instructor",
+                    populate: {
+                        path: "additionDetails",
+                    },
+                }
+            )
+            .populate("category")
+            .populate("ratingAndreviews")
+            .populate(
+                {
+                    path: "courseContent",
+                    populate:{
+                    path: "subSection",
+                    }
+                }
+            )
+           
+        )
+        .exec();
+
+        // validation
+        if(!courseDetails) {
+            return res.status(400).json({
+                success: false,
+                message: `cound not find the curse with ${courseId}`
+            });
+        
+        }
+        // return res
+        return res.status(200).json({
+            success: true,
+            message: "CourseDeatils fetched successfully",
+            data: courseDetails,
+        });
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+        
     }
 }
---
+++ StudyNotion/controllers/RatingAndReview.js
@@ -0,0 +1,123 @@
+const RatindAndReview = require("../models/RatingAndReview");
+const Course = require("../models/Course");
+const RatingAndReview = require("../models/RatingAndReview");
+const { default: mongoose } = require("mongoose");
+
+// createRating
+exports.createRating = async (res, req) => {
+    try{
+        // get user id
+        const userId = res.user.body;
+
+        // fetchdata from req ki body
+        const {rating, review, courseId} = req.body;
+
+        //  check if user is enrolled or not
+        const courseDetails = await Course.findOne(
+            {
+                _id: courseId,
+                studentEnrolled: {$elemMatch: {$eq: userId}},
+            }
+        );
+        if(!courseDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Student is not Enrolled",
+            })
+        }
+
+        // check if user already reviewd the course
+        const alreadyReviewed = await RatindAndReview.findOne({
+                                                    user: userId,
+                                                    course:courseId,
+                                                })
+        if(alreadyReviewed){
+            return res.status(403).json({
+                success: false,
+                message: 'Course is already reviwed by the user'
+            })
+        }
+
+        //  create rating and review
+        const ratingReview = await RatindAndReview.create({
+                                    rating, review,
+                                    course: courseId,
+                                    user: userId,
+                                });
+
+        // update the course with this rating/ review
+        const updatedCourseRecord = await Course.findByIdAndUpdate({_id: courseId},
+                        {
+                            $push: {
+                                ratingAndReview: ratingReview,
+                            }
+                        },
+                        {next: true},
+                    );
+        console.log(updatedCourseRecord);
+
+        // return res
+        return res.status(200).json({
+            success: true,
+            message: "Rating and Reviwed successfully created"
+        })
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+
+    }
+}
+
+
+// GetAvgRAting
+exports.getAverageRating = async (req, res) => {
+    try{
+        // get courseId
+        const courseId = req.courseId;
+
+        // calculate avg rating
+        const result = await RatingAndReview.aggregate([
+            {
+                $match: {
+                    course: new mongoose.Types.ObjectId(courseId),
+                }
+            },
+            {
+                $group:{
+                    _id: null,
+                    averageRating: {$avg: "$rating"},
+                } 
+            }
+        ])
+        // return reating
+        if(result > 0){
+            return res.status(200).json({
+                success: true,
+                averageRating: result[0].averageRating,
+            })
+        }
+
+        // if no reating/ review exist
+        return res.status(200).json({
+            success: true,
+            message: 'Average rating is 0, no rating given till now',
+            averageRating: 0,
+        })
+
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        })  
+    }
+} 
+
+
+
+// getAllRating and review
